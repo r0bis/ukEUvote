f1025af80e5edac516eeec58799aaa0e9d98fc3d
r0bis <roberts.klotins@gmail.com>
2016-06-30 14:16:06 +0100

    latest update

--- bin/graphUpdate.sh
+++ bin/graphUpdate.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# change to project directory
+# get the last graph created
+# sed will perform update in readme.rst according to commands in graphUpdate.sed file
+# this method will avoid backup files; if sed fails for whatever reason - the actual input
+# file is not clobbered
 cd ~/src/ukEUvote
+LASTGRAPH=`ls ~/src/ukEUvote/graphs/ | tail -n 1`
+sed -e '~/src/ukEUvote/bin/graphUpdate.sed' ./readme.rst > ../readme.rst.tmp && mv ../readme.rst.tmp ./readme.rst
