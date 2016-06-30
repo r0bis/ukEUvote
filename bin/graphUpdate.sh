88ab6175975ee351c7fcc343f5cf3eec32a041be
r0bis <roberts.klotins@gmail.com>
2016-06-30 16:56:04 +0100

    latest update

--- bin/graphUpdate.sh
+++ bin/graphUpdate.sh
@@ -4,6 +4,8 @@
 # sed will perform update in readme.rst according to commands in graphUpdate.sed file
 # this method will avoid backup files; if sed fails for whatever reason - the actual input
 # file is not clobbered
+# the correct change relies on regexp in third line; I do not expect the vote will run longer than 2016
+# the correct file name format is determined by the 
 cd ~/src/ukEUvote
 pwd
 LASTGRAPH=`ls ~/src/ukEUvote/graphs/ | tail -n 1`
