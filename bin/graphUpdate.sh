d9993d3a9785962303e30b1a23c8d4a8c0d432ab
r0bis <roberts.klotins@gmail.com>
2016-06-30 14:51:03 +0100

    latest update

--- bin/graphUpdate.sh
+++ bin/graphUpdate.sh
@@ -7,6 +7,6 @@
 cd ~/src/ukEUvote
 pwd
 LASTGRAPH=`ls ~/src/ukEUvote/graphs/ | tail -n 1`
-sed -e s/(votes_2016_[0-9_-]{11}\.png)/$LASTGRAPH/g ./readme.rst > ../readme.rst.tmp && cat ../readme.rst.tmp | echo
+sed 's/(votes_2016_[0-9_-]{11}\.png)/$LASTGRAPH/g' ./readme.rst > ../readme.rst.tmp && cat ../readme.rst.tmp | echo
 
 # mv ../readme.rst.tmp ./readme.rst
