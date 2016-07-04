d3d90377698d3d832c5fb514eb3dde6c1cd5df0a
r0bis <roberts.klotins@gmail.com>
2016-07-04 11:06:10 +0100

    latest update

--- bin/gitAutoPush.sh
+++ bin/gitAutoPush.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# this project runs on full auto
+# this is an explanatory script - needs to be edited to your conditions
+# actual script and backup are run from place not accessible to this project.
+# so how this project runs on full auto
 # run by cron using command crontab -e
 # back up data, get new vote count, create new graph, update graph in readme page, push to git
-# */5 * * * * /home/r0bis/bin/votebackup.sh
-# */5 * * * * /usr/bin/Rscript /home/r0bis/src/ukEUvote/bin/voteBcount.R
-# */30 * * * * /usr/bin/Rscript /home/r0bis/src/ukEUvote/bin/votegraph.R
-# 1-56/5 * * * * /home/r0bis/bin/gitAutoPush.sh
-# 4-59/30 * * * * /home/r0bis/src/ukEUvote/bin/graphUpdate.sh
+# */5 * * * * /home/user/bin/votebackup.sh
+# */5 * * * * /usr/bin/Rscript /home/user/src/ukEUvote/bin/voteBcount.R
+# */30 * * * * /usr/bin/Rscript /home/user/src/ukEUvote/bin/votegraph.R
+# 1-56/5 * * * * /home/user/src/ukEUvote/bin/gitAutoPush.sh
+# 4-59/30 * * * * /home/user/src/ukEUvote/bin/graphUpdate.sh
 
 cd ~/src/ukEUvote
 git pull -q origin master
 git status
 git add .
 git commit -m "latest update"
-git push https://user:password@github.com/r0bis/ukEUvote.git master
+git push https://user:password@github.com/user/ukEUvote.git master
 
 # obviously username and password will be specific to your repository 
